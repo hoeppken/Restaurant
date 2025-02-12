175446d568140b3cd8fffe65647d3118ab15f5da
hoeppken <rafael_rsv@hotmail.com>
2025-02-12 14:56:42 +0100

    new try

--- Restaurant/AboutView.swift
+++ Restaurant/AboutView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView (showsIndicators: false) {
+            VStack(alignment: .leading){
+                
+                Text("About").font(.largeTitle).bold()
+                Image("restaurant-inside").resizable().scaledToFit().padding(.bottom)
+                Text("Sukiyabashi Jiro (すきやばし次郎, Sukiyabashi Jirō) is a sushi restaurant in Ginza, Chūō, Tokyo, owned by Jiro Ono. Ono previously operated as the head chef, but stepped aside in favor of his son Yoshikazu Ono in 2023 due to ill health.Sukiyabashi Jiro was the first sushi restaurant to receive three stars from the Michelin Guide. It was removed from the Michelin Guide in November 2019 as it does not receive reservations from the general public,instead requiring reservations to be made through the concierge of a luxury hotel.")
+                Image("map").resizable().scaledToFit().padding(.bottom)
+                Text("""
+    Address:
+    Basement Floor 1
+    2-15, Ginza 4-chome
+    Chūō, Tokyo
+    """).font(.caption)
+            }.padding(.horizontal)
+        }
+        
+        
+        
     }
 }
 
